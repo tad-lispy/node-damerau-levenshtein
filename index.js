fcd1aac75bf0526674737e39755902ff4894c57b
whyboris <byakubchik@forbes.com>
2019-05-13 14:40:22 +0900

    this that -> firstString secondString

--- index.js
+++ index.js
@@ -2,14 +2,14 @@
 
 // Cache the matrix. Note that if you not pass a limit this implementation will use a dynamically calculate one.
 
-module.exports = function(__this, that, limit) {
+module.exports = function(firstString, secondString, limit) {
 
-  var thisLength = __this.length,
-      thatLength = that.length,
+  var firstLength = firstString.length,
+      secondLength = secondString.length,
       matrix = [];
 
-  // If the limit is not defined it will be calculate from this and that args.
-  limit = (limit || ((thatLength > thisLength ? thatLength : thisLength)))+1;
+  // If the limit is not defined it will be calculate from firstString and secondString args.
+  limit = (limit || ((secondLength > firstLength ? secondLength : firstLength)))+1;
 
   for (var i = 0; i < limit; i++) {
     matrix[i] = [i];
@@ -19,27 +19,27 @@ module.exports = function(__this, that, limit) {
     matrix[0][i] = i;
   }
 
-  if (Math.abs(thisLength - thatLength) > (limit || 100)){
+  if (Math.abs(firstLength - secondLength) > (limit || 100)){
     return prepare (limit || 100);
   }
-  if (thisLength === 0){
-    return prepare (thatLength);
+  if (firstLength === 0){
+    return prepare (secondLength);
   }
-  if (thatLength === 0){
-    return prepare (thisLength);
+  if (secondLength === 0){
+    return prepare (firstLength);
   }
 
   // Calculate matrix.
   var j, this_i, that_j, cost, min, t;
-  for (i = 1; i <= thisLength; ++i) {
-    this_i = __this[i-1];
+  for (i = 1; i <= firstLength; ++i) {
+    this_i = firstString[i-1];
 
     // Step 4
-    for (j = 1; j <= thatLength; ++j) {
+    for (j = 1; j <= secondLength; ++j) {
       // Check the jagged ld total so far
-      if (i === j && matrix[i][j] > 4) return prepare (thisLength);
+      if (i === j && matrix[i][j] > 4) return prepare (firstLength);
 
-      that_j = that[j-1];
+      that_j = secondString[j-1];
       cost = (this_i === that_j) ? 0 : 1; // Step 5
       // Calculate the minimum (much faster than Math.min(...)).
       min    = matrix[i - 1][j    ] + 1; // Deletion.
@@ -47,17 +47,17 @@ module.exports = function(__this, that, limit) {
       if ((t = matrix[i - 1][j - 1] + cost) < min) min = t;   // Substitution.
 
       // Update matrix.
-      matrix[i][j] = (i > 1 && j > 1 && this_i === that[j-2] && __this[i-2] === that_j && (t = matrix[i-2][j-2]+cost) < min) ? t : min; // Transposition.
+      matrix[i][j] = (i > 1 && j > 1 && this_i === secondString[j-2] && firstString[i-2] === that_j && (t = matrix[i-2][j-2]+cost) < min) ? t : min; // Transposition.
     }
   }
 
-  return prepare (matrix[thisLength][thatLength]);
+  return prepare (matrix[firstLength][secondLength]);
 
 /**
  *
  */
   function prepare(steps) {
-    var length = Math.max(thisLength, thatLength)
+    var length = Math.max(firstLength, secondLength)
     var relative = length === 0
       ? 0
       : (steps / length);
